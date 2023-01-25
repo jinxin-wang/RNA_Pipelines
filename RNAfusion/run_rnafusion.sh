9a62a2aa86b425558638d6a88de71b4f4062cdee
Jinxin WANG <wangjinxin.maths@gmail.com>
2023-01-25 17:11:34 +0100

    update

--- RNAfusion/run_rnafusion.sh
+++ RNAfusion/run_rnafusion.sh
@@ -8,4 +8,4 @@ module load nextflow/21.10.6
 
 # nextflow run /home/j_wang@intra.igr.fr/rnafusion/main.nf --allow-setuid --starindex --build_references --all --input samplesheet.csv --outdir ./ --genome GRCh38 -profile singularity -resume
 
-nextflow run /home/j_wang@intra.igr.fr/lib/nfcore/rnafusion/2.1.0/main.nf --starindex --outdir $PWD/20230113_RNAfusion --genome GRCh38 -profile singularity -resume
+nextflow run /home/j_wang@intra.igr.fr/lib/nfcore/rnafusion/2.1.0/main.nf --starindex --outdir $PWD --genome GRCh38 -profile singularity -resume
